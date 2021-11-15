b9a2d01a4b0ae2550eaf6259b3f13f2a68ebb6fc
Federico Rodriguez <federico.rodriguez@intive.com>
2021-11-15 16:20:54 -0300

    styles myWallet were added

--- src/screens/MyWallet/WalletCarrouselStyles.jsx
+++ src/screens/MyWallet/WalletCarrouselStyles.jsx
@@ -1,12 +1,21 @@
 import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
-    wrapperQuestion: {
-        flexDirection: "row", 
-        justifyContent: "center"
-      },
-    question: {
-        fontWeight: "bold",
-        marginBottom: 20,
-      },
-})
+  wrapperQuestion: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  question: {
+    fontWeight: 'bold',
+    marginBottom: 20,
+    fontSize: 18,
+  },
+  carrouselItem: {
+    paddingVertical: 20,
+  },
+  textWithoutMoney: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    textAlign: 'center',
+  },
+});
--- src/screens/MyWallet/index.jsx
+++ src/screens/MyWallet/index.jsx
@@ -1,15 +1,15 @@
-import React, { useEffect, useState, useContext } from 'react';
+import React, { useContext } from 'react';
 import { View } from 'react-native';
 
 import Layout from '../../components/Layout';
 import WalletBalance from './WalletBalance';
 import WalletCarrousel from './WalletCarrousel';
 import { AddRemoveContext } from '../AddRemove/AddRemoveContext';
-import { styles } from '../HomeScreen/styles';
+import { styles } from './styles';
 import { formatNum } from '../../utils/functions/formatNum';
 
 export default function MyWallet() {
-  const { totalMoneyWallet, actualBillsMoneyWallet, initialCoinsMoneyWallet } =
+  const { totalMoneyWallet, initialBillsMoneyWallet, initialCoinsMoneyWallet } =
     useContext(AddRemoveContext);
 
   const handleMoney = (money) => {
@@ -29,14 +29,16 @@ export default function MyWallet() {
       <View style={styles.wrapperBalance}>
         <WalletBalance total={formatNum(totalMoneyWallet)} />
       </View>
-      <WalletCarrousel
-        moneyType="billetes"
-        dataCarrousel={handleMoney(actualBillsMoneyWallet)}
-      />
-      <WalletCarrousel
-        moneyType="monedas"
-        dataCarrousel={handleMoney(initialCoinsMoneyWallet)}
-      />
+      <View style={styles.moneyContainer}>
+        <WalletCarrousel
+          moneyType="billetes"
+          dataCarrousel={handleMoney(initialBillsMoneyWallet)}
+        />
+        <WalletCarrousel
+          moneyType="monedas"
+          dataCarrousel={handleMoney(initialCoinsMoneyWallet)}
+        />
+      </View>
     </Layout>
   );
 }
