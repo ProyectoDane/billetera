109f99eded6c5ea29f61b7616acbb10c0097a49d
Federico Rodriguez <federico.rodriguez@intive.com>
2021-11-15 15:19:54 -0300

    description function toast

--- src/utils/functions/toastNotifcation.js
+++ src/utils/functions/toastNotifcation.js
@@ -1,5 +1,10 @@
 import { showMessage } from 'react-native-flash-message';
 
+// Options toast notificacion
+// Message: Description for notification.
+// Type: "success" (green), "warning" (orange), "danger" (red), "info" (blue) and "default" (gray).
+// Icon: "none" (default), "auto" (guided by type), "success", "info", "warning", "danger"
+
 export const toastNotification = (message, type, icon) => {
   showMessage({
     message: message,
