ae27fb19c0113d3d53e5e7e24c3e86b44d6aee1f
Juan Alvarez <juan.alvarez@intive-fdv.com>
2021-11-16 19:27:30 -0300

    funcion que calcula como pagar

---
+++ src/utils/functions/calculaComoPagar.js
@@ -0,0 +1,149 @@
+let money = [{
+    "id": 13,
+    "quantity": 10,
+    "moneyId": 13,
+    "userId": 1,
+    "image": "1000Hornero.png",
+    "amount": 1000,
+    "isCoins": 0
+}, {
+    "id": 12,
+    "quantity": 2,
+    "moneyId": 12,
+    "userId": 1,
+    "image": "500Yaguarete.png",
+    "amount": 500,
+    "isCoins": 0
+}, {
+    "id": 11,
+    "quantity": 2,
+    "moneyId": 11,
+    "userId": 1,
+    "image": "200.png",
+    "amount": 200,
+    "isCoins": 0
+}, {
+    "id": 10,
+    "quantity": 2,
+    "moneyId": 10,
+    "userId": 1,
+    "image": "100Taruca.png",
+    "amount": 100,
+    "isCoins": 0
+}, {
+    "id": 10,
+    "quantity": 5,
+    "moneyId": 10,
+    "userId": 1,
+    "image": "5pe.png",
+    "amount": 5,
+    "isCoins": 0
+}, {
+    "id": 10,
+    "quantity": 5,
+    "moneyId": 10,
+    "userId": 1,
+    "image": "5pe_moneda.png",
+    "amount": 5,
+    "isCoins": 1
+}
+    , {
+        "id": 10,
+        "quantity": 10,
+        "moneyId": 10,
+        "userId": 1,
+        "image": "1pe.png",
+        "amount": 1,
+        "isCoins": 0
+    }]
+
+function copyMoney(moneyEntry, quantity) {
+    let copy = {...moneyEntry};
+    copy.quantity = quantity;
+    return copy;
+}
+
+function getTotales(billetes) {
+    return billetes.reduce((acum, current) => {
+        let cantBilletes = acum.cantBilletes + (current.isCoins ? 0 : (1 * current.quantity));
+        let cantMonedas = acum.cantMonedas + (current.isCoins ? (1 * current.quantity) : 0);
+        return {
+            amount: acum.amount + current.amount * current.quantity,
+            cantBilletes: cantBilletes,
+            cantMonedas: cantMonedas,
+        };
+    }, {amount: 0, cantBilletes: 0, cantMonedas: 0});
+}
+
+function pagarCon(amount, myMoney) {
+
+    let totalEnBilletera = myMoney.reduce((acum, current) => {
+        return acum + current.amount * current.quantity;
+    }, 0);
+
+    if (totalEnBilletera < amount)
+        return [];
+
+    let resultados = [];
+
+    for (let [index, moneyEntry] of myMoney.entries()) {
+        // your code goes here
+        const moneyEntry = myMoney[index];
+        if (moneyEntry.amount >= amount) {
+            resultados.push({
+                billetes: [copyMoney(moneyEntry, 1)],
+                vuelto: moneyEntry.amount - amount
+            });
+        } else {
+
+            //Lo que hay que pagar no entra en el billete
+
+            let veces = Math.trunc(amount / moneyEntry.amount);
+
+            if (veces >= 1) {
+                let cantidadBilletes = Math.min(veces, moneyEntry.quantity)
+
+                let vuelto = amount % (moneyEntry.amount * cantidadBilletes);
+
+                if (vuelto > 0 && moneyEntry.quantity > veces) {
+                    let auxBilletes = [copyMoney(moneyEntry, cantidadBilletes + 1)];
+                    let newResultado = {
+                        billetes: auxBilletes,
+                        vuelto: moneyEntry.amount * (cantidadBilletes + 1) - amount,
+                        totales: getTotales(auxBilletes)
+                    };
+                    resultados.push(newResultado);
+                }
+
+                let ret = pagarCon(amount - moneyEntry.amount * cantidadBilletes, myMoney.slice(index + 1));
+                //Ret tiene un monton de resultados posibles, cada resultado se le agrega al resultado parcial actual
+
+                for (const unRet of ret) {
+                    let billetes = [copyMoney(moneyEntry, cantidadBilletes), ...(unRet.billetes || [])];
+                    let totales = getTotales(billetes);
+                    let newResultado = {
+                        billetes: billetes,
+                        vuelto: totales.amount - amount,
+                        totales: totales
+                    };
+                    resultados.push(newResultado);
+
+                }
+
+
+            }
+
+
+        }
+        console.log("");
+    }
+
+
+    return resultados;
+}
+
+//let resultado = pagarCon(4001, money);
+//console.log(JSON.stringify(resultado, null, 2));
+//pagarCon(10, money);
+
+export default pagarCon;
