4e94cc723ef4c5a722e000664142a5dafd74448e
Leandro Favre <leandro.favre@intive.com>
2022-11-08 12:02:23 -0300

    Text component unused removed

--- src/screens/Onboarding/Onboarding.jsx
+++ src/screens/Onboarding/Onboarding.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StatusBar, View, Text } from 'react-native';
+import { StatusBar, View } from 'react-native';
 
 import OnboardSlide from '../../components/OnboardSlide';
 
