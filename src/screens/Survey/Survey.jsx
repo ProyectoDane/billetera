dc72cf0b55a815e6990bdc705f564fcc022740dc
Leandro Favre <leandro.favre@intive.com>
2022-11-10 14:07:25 -0300

    chore: refactor done.

--- src/screens/Survey/Survey.jsx
+++ src/screens/Survey/Survey.jsx
@@ -33,7 +33,7 @@ const Survey = ({ navigation, route }) => {
     return unsuscribe;
   },[navigation]);
 
-  useEffect(() => {
+  useEffect(async () => {
     const init = async () => {
       const isDone = await surveyDone(currentUser.id);
       if (!isDone) {
