53a0e5fe3be7bf7abf2745144f8e50d12e1855e0
Leandro Favre <leandro.favre@intive.com>
2022-11-11 18:26:10 -0300

    chore: new components ItemWishDone, IconContainer. Updated ItemWish to sync with Figma

--- src/screens/MyWishes/components/ItemWish/ItemWish.jsx
+++ src/screens/MyWishes/components/ItemWish/ItemWish.jsx
@@ -10,6 +10,7 @@ import { styles } from './styles';
 import { formatNum } from '../../../../utils/functions/formatNum';
 import { toastNotification } from '../../../../utils/functions/toastNotifcation';
 import { AddRemoveContext } from '../../../AddRemove/AddRemoveContext';
+import ItemWishDone from './ItemWishDone';
 
 const ItemWish = ({ name, value, wishId, testID, icon, done, onChange }) => {
   const [collapse, setCollapse] = useState(false);
@@ -77,95 +78,70 @@ const ItemWish = ({ name, value, wishId, testID, icon, done, onChange }) => {
   };
 
   return (
-    <View style={{ ...styles.container, padding: 10 }}>
-      <View style={{ ...styles.item, flex: 1, padding: 10 }}>
-        <View style={styles.itemTopSection}>
-          <View style={{ flex: 0.2 }}>
-            <View style={styles.iconItem}>
-              <FontAwesome5 name={icon} size={25} color={colors.primary} />
+    <>
+      {!done ? (
+        <View style={{ ...styles.container, padding: 10 }}>
+          <View style={{ ...styles.item, flex: 1, padding: 10 }}>
+            <View style={styles.itemTopSection}>
+              <View style={{ flex: 0.2 }}>
+                <View style={styles.iconItem}>
+                  <FontAwesome5 name={icon} size={25} color={colors.primary} />
+                </View>
+              </View>
+              <View style={{ ...styles.dataItem, flex: 1.2, paddingLeft: 25 }}>
+                <Progress.Bar
+                  color={colors.primary}
+                  unfilledColor={colors.primarySoft}
+                  progress={progress}
+                  width={null}
+                  height={15}
+                  borderWidth={0}
+                  borderRadius={10}
+                />
+              </View>
+            </View>
+
+            <View style={{ flexDirection: 'column', flex: 1 }}>
+              <View style={styles.itemTextRow}>
+                <Text style={styles.itemLabel}>{name}</Text>
+                <Text style={styles.valueItem}>{formatNum(value)}</Text>
+              </View>
+              {!done ? (
+                <>
+                  <View style={styles.itemTextRow}>
+                    <Text style={styles.itemLabel}>TENES AHORRADO:</Text>
+                    <Text style={styles.valueItem}>{formatNum(totalMoneySavings)}</Text>
+                  </View>
+                  <View style={styles.itemTextRow}>
+                    <Text style={styles.itemLabel}>TE FALTAN:</Text>
+                    <Text style={{ ...styles.valueItem, ...styles.valueItemSpecial }}>{formatNum(missingMoney)}</Text>
+                  </View>
+                </>
+              ) : null}
+            </View>
+            <View>
+              <View style={{ ...styles.actionsContainer, ...(done ? { justifyContent: 'flex-end' } : {}) }}>
+                {!done ? (
+                  <TouchableOpacity onPress={handleAchieve}>
+                    <Text style={{ ...styles.actionBtn, ...textColor }}>CUMPLIR</Text>
+                  </TouchableOpacity>
+                ) : null}
+                {!done ? (
+                  <TouchableOpacity onPress={handleEdit}>
+                    <Text style={{ ...styles.actionBtn, ...editTextColor }}>EDITAR</Text>
+                  </TouchableOpacity>
+                ) : null}
+                <TouchableOpacity onPress={handleDelete}>
+                  <Text style={{ ...styles.actionBtn }}>ELIMINAR</Text>
+                </TouchableOpacity>
+              </View>
             </View>
           </View>
-          <View style={{ ...styles.dataItem, flex: 1.2, paddingLeft: 25 }}>
-            {!done && (
-              <Progress.Bar
-                color={colors.primary}
-                unfilledColor={colors.primarySoft}
-                progress={progress}
-                width={null}
-                height={15}
-                borderWidth={0}
-                borderRadius={10}
-              />
-            )}
-          </View>
         </View>
-
-        <View style={{ flexDirection: 'column', flex: 1 }}>
-          <View style={styles.itemTextRow}>
-            <Text style={styles.itemLabel}>{name}</Text>
-            <Text style={styles.valueItem}>{formatNum(value)}</Text>
-          </View>
-          {!done ? (
-            <>
-              <View style={styles.itemTextRow}>
-                <Text style={styles.itemLabel}>TENES AHORRADO:</Text>
-                <Text style={styles.valueItem}>{formatNum(totalMoneySavings)}</Text>
-              </View>
-              <View style={styles.itemTextRow}>
-                <Text style={styles.itemLabel}>TE FALTAN:</Text>
-                <Text style={{ ...styles.valueItem, ...styles.valueItemSpecial }}>{formatNum(missingMoney)}</Text>
-              </View>
-            </>
-          ) : null}
-        </View>
-        <View>
-          <View style={{ ...styles.actionsContainer, ...(done ? { justifyContent: 'flex-end' } : {}) }}>
-            {!done ? (
-              <TouchableOpacity onPress={handleAchieve}>
-                <Text style={{ ...styles.actionBtn, ...textColor }}>CUMPLIR</Text>
-              </TouchableOpacity>
-            ) : null}
-            {!done ? (
-              <TouchableOpacity onPress={handleEdit}>
-                <Text style={{ ...styles.actionBtn, ...editTextColor }}>EDITAR</Text>
-              </TouchableOpacity>
-            ) : null}
-            <TouchableOpacity onPress={handleDelete}>
-              <Text style={{ ...styles.actionBtn }}>ELIMINAR</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
-      </View>
-      {/*{true && (*/}
-      {/*  <View style={styles.collapse}>*/}
-      {/*    {!done ? (*/}
-      {/*      <>*/}
-      {/*        <Text style={styles.itemLabel}>*/}
-      {/*          TENES AHORRADO: {formatNum(totalMoneySavings)}*/}
-      {/*        </Text>*/}
-      {/*        <Text style={styles.itemLabel}>*/}
-      {/*          TE FALTAN: {formatNum(missingMoney)}*/}
-      {/*        </Text>*/}
-      {/*      </>*/}
-      {/*    ) : null}*/}
-      {/*    <View style={styles.actionsContainer}>*/}
-      {/*      <TouchableOpacity*/}
-      {/*        disabled={done === 0 ? false : true}*/}
-      {/*        onPress={handleAchieve}>*/}
-      {/*        <Text style={[styles.actionBtn, textColor]}>CUMPLIR</Text>*/}
-      {/*      </TouchableOpacity>*/}
-      {/*      <TouchableOpacity*/}
-      {/*        disabled={done === 1 ? true : false}*/}
-      {/*        onPress={handleEdit}>*/}
-      {/*        <Text style={[styles.actionBtn, editTextColor]}>EDITAR</Text>*/}
-      {/*      </TouchableOpacity>*/}
-      {/*      <TouchableOpacity onPress={handleDelete}>*/}
-      {/*        <Text style={styles.actionBtn}>ELIMINAR</Text>*/}
-      {/*      </TouchableOpacity>*/}
-      {/*    </View>*/}
-      {/*  </View>*/}
-      {/*)}*/}
-    </View>
+      ) : (        
+        <ItemWishDone name={name} value={value} icon={icon} onDelete={handleDelete} />
+      )}
+    </>
   );
 };
 
