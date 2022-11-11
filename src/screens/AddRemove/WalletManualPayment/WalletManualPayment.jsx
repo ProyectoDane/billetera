b26741ca266d42aa5de1c23d419beb258b435c2d
Juan Alvarez <juan.alvarez@intive-fdv.com>
2022-11-10 23:04:18 -0300

    Manual Payment Wallet L&F

--- src/screens/AddRemove/WalletManualPayment/WalletManualPayment.jsx
+++ src/screens/AddRemove/WalletManualPayment/WalletManualPayment.jsx
@@ -1,6 +1,6 @@
 import React, {useContext, useState} from 'react';
-import {Text, useWindowDimensions, View} from 'react-native';
-import {TabView} from 'react-native-tab-view';
+import {StyleSheet, Text, useWindowDimensions, View} from 'react-native';
+import {TabBar, TabView} from 'react-native-tab-view';
 
 import SingleButton from '../../../components/SingleButton';
 import {colors, SCREEN_NAME} from '../../../constants';
@@ -12,6 +12,13 @@ import {ManualPaymentContext} from '../ManualPaymentContext';
 import {innerSaveManualPayment} from '../utils';
 import WalletManualPaymentBills
   from './WalletManualPaymentBills/WalletManualPaymentBills';
+import {FontAwesome5} from "@expo/vector-icons";
+import { styles as commonStyles } from '../commonAddRemoveStyles';
+import {styles as myWishesStyles} from "../../MyWishes/styles";
+import {shadow} from "../../../constants/styles";
+import { default as cardStyles} from "../../../components/Card/styles";
+import {styles as itemWishStyles} from "../../MyWishes/components/ItemWish/styles";
+import {styles as WishesStyles} from "../../WishesHome/styles";
 
 export default function WalletManualPayment({ navigation }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -98,33 +105,78 @@ export default function WalletManualPayment({ navigation }) {
     navigation.navigate(SCREEN_NAME.HOME);
   };
 
+  const getTabBarIcon = (props) => {
+    const { route } = props;
+    if (route.key === 'first') {
+      return <FontAwesome5 name="money-bill-wave" size={20} color={colors.primary} />;
+    } else {
+      return <FontAwesome5 name="coins" size={20} color={colors.primary} />;
+    }
+  };
+
+
+
   return (
     <View style={{ flex: 1 }}>
-      <View style={{backgroundColor: "white", marginHorizontal: 5}}>
-
-        <View>
-          <Text style={{fontSize: 30, textAlign: 'right'}}>
-            TOTAL {formatNum(totalFreeze)}
-          </Text>
+      <View
+          style={{
+            ...cardStyles.card,
+            margin: 10,
+            width: "auto",
+            flex: 1,
+            flexBasis: 60,
+            paddingVertical: 10,
+            // flexShrink: 1,
+            flexGrow: 0,
+            ...shadow,
+          }}>
+        <View style={{...itemWishStyles.itemTextRow}}>
+          <Text style={{...itemWishStyles.itemLabel}}>TOTAL</Text>
+          <Text style={{...itemWishStyles.valueItem}}>{formatNum(totalFreeze)}</Text>
         </View>
-        <View>
-        </View>
-        <Text style={{fontSize: 30, textAlign: 'right'}}>
-          {totalPaymentWallet > 0
-              ? `TE FALTA PAGAR ${formatNum(totalPaymentWallet)}`
-              : totalPaymentWallet === 0
-                  ? `PAGASTE JUSTO`
-                  : `TU VUELTO ES ${formatNum(Math.abs(totalPaymentWallet))}`}
-        </Text>
+        {totalPaymentWallet > 0 ? (
+          <View style={{...itemWishStyles.itemTextRow}}>
+            <Text style={{...itemWishStyles.itemLabel}}>TE FALTA PAGAR</Text>
+            <Text style={{...itemWishStyles.valueItem}}>{formatNum(totalPaymentWallet)}</Text>
+          </View>
+        ) : (
+        totalPaymentWallet === 0 ? (
+          <View style={{...itemWishStyles.itemTextRow}}>
+            <Text style={{...itemWishStyles.itemLabel}}>PAGASTE JUSTO</Text>
+          </View>
+        ) : (
+            <View style={{...itemWishStyles.itemTextRow}}>
+              <Text style={{...itemWishStyles.itemLabel}}>TU VUELTO ES </Text>
+              <Text style={{...itemWishStyles.valueItem,
+                ...itemWishStyles.valueItemSpecial}}>{formatNum(Math.abs(totalPaymentWallet))}</Text>
+            </View>
+        )
+        ) }
       </View>
-      <TabView
-        navigationState={{ index, routes }}
-        renderScene={renderScene}
-        onIndexChange={setIndex}
-        initialLayout={{ width: layout.width }}
 
-      />
-      <View style={{ paddingVertical: 0 }}>
+      <View style={{...cardStyles.card, ...shadow, flex: 15, margin: 10, width: "auto", flexGrow: 1,}}>
+        <TabView
+          navigationState={{ index, routes }}
+          renderScene={renderScene}
+          onIndexChange={setIndex}
+          initialLayout={{ width: layout.width }}
+          renderTabBar={(props) => (
+              <TabBar
+                  {...props}
+                  indicatorStyle={commonStyles.indicatorStyle}
+                  style={{ backgroundColor: colors.white}}
+                  renderIcon={(props) => getTabBarIcon(props)}
+                  tabStyle={commonStyles.tabStyle}
+                  labelStyle={commonStyles.tabLabel}
+              />)}
+
+        />
+      </View>
+      <View
+          style={{
+            ...myWishesStyles.bottomButtonContainer,
+            paddingVertical: 0,
+          }}>
         <SingleButton
           icon="money-bill-wave"
           sizeIcon={22}
@@ -136,14 +188,13 @@ export default function WalletManualPayment({ navigation }) {
           onPress={handleSave}
           style={{
             width: "100%",
-            height: 50,
-            marginTop: 0,
             backgroundColor:
               totalPaymentWallet !== 0 && totalPaymentWallet > 0
                 ? colors.disable
-                : colors.secondary,
+                : colors.primary,
           }}
         />
+
       </View>
     </View>
   );
