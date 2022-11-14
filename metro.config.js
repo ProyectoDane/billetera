2ef35869b81458fcd6834680e712a571011025f5
Juan Alvarez <juan.alvarez@intive-fdv.com>
2022-11-14 01:22:16 -0300

    dependencies for expo SDK 47 metro.config.js for SVG

--- metro.config.js
+++ metro.config.js
@@ -1,19 +1,21 @@
-const { getDefaultConfig } = require('metro-config');
+// Learn more https://docs.expo.io/guides/customizing-metro
+const { getDefaultConfig } = require('expo/metro-config');
+
 
 module.exports = (async () => {
-    const {
-        resolver: {
-            sourceExts,
-            assetExts
-        }
-    } = await getDefaultConfig();
+    const config = await getDefaultConfig(__dirname);
+    const { transformer, resolver } = config;
 
-    return {
-        transformer: {
-            babelTransformerPath: require.resolve("react-native-svg-transformer")
-        },
-        resolver: {
-            assetExts: assetExts.filter(ext => ext !== "svg"),
-            sourceExts: [...sourceExts, "svg"]
-        }};
+    config.transformer = {
+        ...transformer,
+        babelTransformerPath:  require.resolve("react-native-svg-transformer")
+    };
+
+    config.resolver = {
+        ...resolver,
+        assetExts: resolver.assetExts.filter(ext => ext !== "svg"),
+        sourceExts: [...resolver.sourceExts, 'svg'],
+    };
+
+    return config;
 })();
