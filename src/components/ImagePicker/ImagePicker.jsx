21d770498e90e7210c648a3a166c9e32f07d3255
Juan Alvarez <juan.alvarez@intive-fdv.com>
2022-11-15 23:50:02 -0300

    update imagePicker deprecated properties

--- src/components/ImagePicker/ImagePicker.jsx
+++ src/components/ImagePicker/ImagePicker.jsx
@@ -23,8 +23,8 @@ const ImagePicker = ({setImage}) => {
             quality: 1,
         });
 
-        if (!result.cancelled) {
-            setImage(result.uri);
+        if (!result.canceled) {
+            setImage(result.assets[0].uri);
         }
     };
 
