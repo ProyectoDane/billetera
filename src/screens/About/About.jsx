9bebd7860c18387c9d36a89567261cc37c893a95
Juan Alvarez <juan.alvarez@intive-fdv.com>
2022-12-27 11:16:30 -0300

    About screen L&F

--- src/screens/About/About.jsx
+++ src/screens/About/About.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {
-  Image,
-  Linking,
-  Text,
-  TouchableOpacity,
-  useWindowDimensions,
-  View
-} from 'react-native';
+import {Image, Linking, Text, TouchableOpacity, useWindowDimensions, View} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 
 import daneLogo from '../../../assets/dane_logo.png';
 import tincLogo from '../../../assets/tinc_logo.png';
 import intiveLogo from '../../../assets/intive_logo_blue.png';
 
-import {labels} from '../../constants';
+import {colors, labels} from '../../constants';
 import {styles} from './styles';
 import {MarkedList} from "@jsamr/react-native-li";
 import {disc} from "@jsamr/counter-style/presets";
@@ -21,13 +14,13 @@ import {FontAwesome5} from "@expo/vector-icons";
 import TextUppercase from "../../components/TextUppercase";
 
 const About = () => {
-  const width = useWindowDimensions().width - 20;
+  const width = useWindowDimensions().width - 50;
   const dummy = () => {};
   const imageGenerator = (img, height, url) => {
     const openLink = () => (url?Linking.openURL(url):dummy);
     return (
         <TouchableOpacity onPress={openLink}>
-          <View style={styles.imageContainer}>
+          <View style={{...styles.imageContainer,}}>
             <Image source={img} style={{width, height}} resizeMode="contain"/>
           </View>
         </TouchableOpacity>
@@ -54,40 +47,51 @@ const About = () => {
   ];
 
 
+  const hr = <View style={{
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    height: 1,
+    backgroundColor: colors.softGray,
+    marginVertical: 15
+  }}></View>;
   return (
-    <ScrollView style={styles.whiteContainer}>
-      <View style={{...styles.container, paddingVertical: 20, marginBottom: 20, paddingHorizontal: 5}}>
-        {imageGenerator(daneLogo, width / 6, "http://www.proyectodane.org/")}
-
-        <TextUppercase bold style={styles.title}>{labels.aboutScreen.dane}</TextUppercase>
-        <Text paragraph>{labels.aboutScreen.descripton}</Text>
+    <ScrollView style={{}}>
+      <View style={{...styles.container}}>
+        <TextUppercase bold style={{...styles.title, marginBottom: 10}}>{labels.aboutScreen.dane}</TextUppercase>
+        {imageGenerator(daneLogo, width / 5, "http://www.proyectodane.org/")}
+        <Text paragraph style={{marginTop: 10}}>{labels.aboutScreen.descripton}</Text>
+        {hr}
         <TextUppercase bold style={styles.title}>{labels.aboutScreen.coordination}</TextUppercase>
         {imageGenerator(tincLogo, width / 3, "https://tinc.org.ar/")}
-        <TextUppercase bold style={styles.title}>{labels.aboutScreen.idea}</TextUppercase>
-        <Text paragraph>{labels.aboutScreen.ideaText}</Text>
+        {hr}
+        {/*<TextUppercase bold style={styles.title}>{labels.aboutScreen.idea}</TextUppercase>*/}
+        {/*<Text paragraph>{labels.aboutScreen.ideaText}</Text>*/}
         <TextUppercase bold style={styles.title}>{labels.aboutScreen.development}</TextUppercase>
         <View>
           {imageGenerator(intiveLogo, width / 8, "https://intive.com/")}
           <MarkedList counterRenderer={disc}>
-            {members.map((elem, index)=>(
-                <Text key={index} style={{ flexShrink: 2, marginVertical: 3 }}  onPress={() => Linking.openURL(elem.url)}>
-                  {elem.name} <FontAwesome5  name='linkedin' />
+            {members.map((elem, index) => (
+                <Text key={index} style={{flexShrink: 2, marginVertical: 3}} onPress={() => Linking.openURL(elem.url)}>
+                  {elem.name} <FontAwesome5 name='linkedin'/>
                 </Text>
             ))}
           </MarkedList>
         </View>
+        {hr}
         <TextUppercase bold style={styles.title}>{labels.aboutScreen.acknowledgment}</TextUppercase>
         <View>
           <MarkedList counterRenderer={disc}>
-            {thanksTo.map((elem, index)=>(
-                <Text key={index} style={{ flexShrink: 2, marginVertical: 3 }}  onPress={() => Linking.openURL(elem.url)}>
-                  {elem.name} <FontAwesome5  name='linkedin' />
+            {thanksTo.map((elem, index) => (
+                <Text key={index} style={{flexShrink: 2, marginVertical: 3}} onPress={() => Linking.openURL(elem.url)}>
+                  {elem.name} <FontAwesome5 name='linkedin'/>
                 </Text>
             ))}
           </MarkedList>
         </View>
+        {hr}
         <TextUppercase bold style={styles.title}>{labels.aboutScreen.license}</TextUppercase>
-        <Text style={{ textTransform: 'lowercase' }}  onPress={() => Linking.openURL(labels.aboutScreen.licenseLink)}>
+        <Text style={{textTransform: 'lowercase'}} onPress={() => Linking.openURL(labels.aboutScreen.licenseLink)}>
           {labels.aboutScreen.licenseLink}
         </Text>
       </View>
--- src/screens/About/styles.jsx
+++ src/screens/About/styles.jsx
@@ -1,22 +1,24 @@
-import { StyleSheet } from 'react-native';
+import {StyleSheet} from 'react-native';
 
-import { colors } from '../../constants';
+import {colors} from '../../constants';
+import {shadow} from "../../constants/styles";
 
 export const styles = StyleSheet.create({
-  whiteContainer: {
-    backgroundColor: colors.white,
-  },
-  container: {
-    flex: 1,
-    marginHorizontal: 10,
-  },
-  imageContainer: {
-    marginVertical: 10,
-    alignContent: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    marginTop: 15,
-    marginBottom: 5
-  }
+    whiteContainer: {
+        backgroundColor: colors.white,
+    }, container: {
+        flex: 1,
+        marginHorizontal: 10,
+        backgroundColor: colors.white,
+        borderRadius: 15,
+        paddingVertical: 20,
+        marginBottom: 20,
+        paddingHorizontal: 15,
+        marginVertical: 10,
+        ...shadow,
+    }, imageContainer: {
+        marginVertical: 10, alignContent: 'center', justifyContent: 'center',
+    }, title: {
+        marginTop: 0, marginBottom: 10
+    }
 });
